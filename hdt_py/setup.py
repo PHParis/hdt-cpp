2360a10f39b574daf0ec9408dba1885b86925451
PHParis <pierrehenri.paris@gmail.com>
2019-03-27 14:37:37 +0100

    version 0.0.2

--- hdt_py/setup.py
+++ hdt_py/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hdt_py',
-      version='0.0.1',
+      version='0.0.2',
       description='HDT file manager',
       url='http://github.com/PHParis/hdt-cpp',
       author='PHParis',
